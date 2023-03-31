cad050e22e69dbaefe03c1cdd46586a11e32a62d
Nastja <nastasjaandre@gmail.com>
2023-03-31 18:17:00 +0300

    added task*

---
+++ from_sem_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача, оставшаяся с семинара:
+// * Напишите программу вычисления модуля числа.
+// 2 -> 2
+// -3 -> 3
+// -7 -> 7
+
+System.Console.Write("Input a number: ");
+string value;
+value = Console.ReadLine();
+int number = Convert.ToInt32(value);
+
+int numAbs = System.Math.Abs(number);
+
+System.Console.Write($"Absolute value of {number} is {numAbs}.");
