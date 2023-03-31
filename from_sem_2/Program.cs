1736d7851b4c0a48aa6e7e4b941581c9895e0b24
Nastja <nastasjaandre@gmail.com>
2023-03-31 18:25:00 +0300

    added task**

---
+++ from_sem_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача, оставшаяся с семинара:
+// ** Напишите программу, которая на вход принимает значение, а на выходе показывает обратное значение.
+// 1 -> 1
+// 2 -> 0.5
+// 0.25 -> 4
+
+System.Console.Write("Input a number: ");
+string value;
+value = Console.ReadLine();
+double number = Convert.ToDouble(value);
+
+double inversedNum = 1 / number;
+
+System.Console.Write($"Inversed number of {number} is {inversedNum}.");
