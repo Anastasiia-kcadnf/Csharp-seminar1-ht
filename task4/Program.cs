49914e19122ad0a99f37273ca0b19fe481f22bef
Nastja <nastasjaandre@gmail.com>
2023-03-31 18:56:51 +0300

    added task4

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.Write("Input a number: ");
+string value;
+value = Console.ReadLine();
+int number = Convert.ToInt32(value);
+
+int numAbs = System.Math.Abs(number);
+
+int current = 2;
+
+if(numAbs < 2)
+{
+    System.Console.Write($"There is no even numbers from 1 to {numAbs}");
+}
+else
+{
+    while(current <= numAbs)
+    {
+        System.Console.Write(current + " ");
+        current += 2;
+    }
+}
